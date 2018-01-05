56345658308d1ca6bfe5a980dad3a6620552ccb4
Leoliyiang <31967637+Leoliyiang@users.noreply.github.com>
2018-01-05 15:12:32 -0500

    test

--- 24points.py
+++ 24points.py
@@ -1,3 +1,17 @@
+import sys
+
+def test(did_pass):
+    """  Print the result of a test.  """
+    linenum = sys._getframe(1).f_lineno   # Get the caller's line number.
+    if did_pass:
+        msg = "Test at line {0} ok.".format(linenum)
+    else:
+        msg = ("Test at line {0} FAILED.".format(linenum))
+    print(msg)
+    
+def test_suite():
+    test(twenty_four_points()==True)
+
 import random
 def twenty_four_points():
     a=random.randint(1,13)
@@ -9,7 +23,7 @@ def twenty_four_points():
     num3=int(c)
     num4=int(d)
     print(num1,num2,num3,num4)
-    a1=input("The first number represents num1, the second number represents num2... Use numx to calculate 24!")
+    a1=int(input("The first number represents num1, the second number represents num2... Use numx to calculate 24!"))
     
     if a1==24:
         return True
@@ -17,4 +31,5 @@ def twenty_four_points():
         twenty_four_points()
     
     
-twenty_four_points()
+#twenty_four_points()
+test_suite()
