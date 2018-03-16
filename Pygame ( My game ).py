c57ff1425d2aec5d79de420a3edf9a74a8f74c3d
Leoliyiang <31967637+Leoliyiang@users.noreply.github.com>
2018-03-16 14:47:31 -0400

    s

--- Pygame ( My game ).py
+++ Pygame ( My game ).py
@@ -1,12 +1,24 @@
 import pygame
 from pygame.locals import *
+display_width = 800
+display_height = 600
 
-SCREENRECT = Rect(0, 0, 640, 480)
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('matchstick')
+clock = pygame.time.Clock()
+
+matchImg = pygame.image.load('matchstick.jpg')
+
+def match_draw(x,y,count=5):
+    for i in range(count):
+        gameDisplay.blit(pygame.transform.scale(matchImg,(10,100)),(x+i*20,y))
+
 def main(winstyle=0):
     pygame.init()
-    winstyle=0
-    bestdepth = pygame.display.mode_ok(SCREENRECT.size,winstyle,32)
-    screen = pygame.display.set_mode(SCREENRECT.size,winstyle,bestdepth)
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +26,16 @@ def main(winstyle=0):
                 (event.type == KEYDOWN and event.key == K_ESCAPE):
                     return
         keystate = pygame.key.get_pressed()
+
+        match_draw(display_width / 2 - 300, display_height / 2 - 300, 20)
+        match_draw(display_width/2,display_height/2,20)
+        match_draw(display_width / 2 + 200, display_height / 2 + 200, 20)
+        pygame.display.update()
+
+
+
+
+
 main()
 pygame.quit()
 quit()
